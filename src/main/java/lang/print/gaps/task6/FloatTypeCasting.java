650106f29ed54a5e6b4c01142b6418d2249b6e24
Ihor Chernomorets <oxnardxvi@gmail.com>
2023-11-28 17:43:31 +0100

    Initial commit

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        float fract = numberToBeRounded - (int)numberToBeRounded;
+        int rounded = (int)numberToBeRounded + (fract < 0.5 ? 0 : 1);
+
+        System.out.println(rounded);
     }
 }
